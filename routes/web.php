e9d083e9e32c5f8315f4170bbe737c52e3f8e1f3
aliful05 <surabayaoioi02@gmail.com>
2021-06-05 14:55:36 +0700

    praktikum3

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('dokter',ControllerDokter::class);
+Route::resource('/dokter', 'App\Http\Controllers\DokterController');
